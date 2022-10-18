59c80e5e79801452178af2e0121c437b6c1a465c
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-18 13:31:31 -0400

    React buttons and functions.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('Hello, friend.', e);
+  };
+
+  const handleClickInstead = (name, e) => {
+    console.log('Hello, ' + name + '!', e.target);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => {handleClickInstead('Mario', e)}}>Click me instead.</button>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,7 @@ const Navbar = () => {
       <h1>The Plicky Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create" style={{
-          color: "white",
-          backgroundColor: 'rgba(50, 205, 50, 100)',
-          borderRadius: '8px'
-        }}>New Blog</a>
+        <a href="/create">New Blog</a>
       </div>
     </nav>
   );
