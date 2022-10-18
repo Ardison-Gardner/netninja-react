ffd01bc5c3ce9eba8268dac6ff737dcf0a9ffb1f
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-18 16:00:50 -0400

    Passing string as props.

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,11 @@
 const BlogList = (props) => {
 
   const blogs = props.blogs;
+  const title = props.title;
 
   return (
     <div className="blogList">
+      <h2>{ title }</h2>
       {blogs.map((blog) => (
         <div className="blogPreview" key={ blog.id }>
           <h2>{ blog.title }</h2>
