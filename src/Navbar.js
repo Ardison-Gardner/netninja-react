7e6f86401a26ad38528f678212b1934c1efe235a
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-18 13:18:50 -0400

    Inline styling through React.

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
       <h1>The Plicky Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" style={{
+          color: "white",
+          backgroundColor: 'rgba(50, 205, 50, 100)',
+          borderRadius: '8px'
+        }}>New Blog</a>
       </div>
     </nav>
   );
