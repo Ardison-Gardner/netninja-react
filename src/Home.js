0b8f6863ae5d6b01bdf8ce11aad58f79ba1a39cb
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-20 16:52:54 -0400

    Error catching and throwing.

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,37 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then(res => {
-        return res.json()
+        if (!res.ok) {
+          throw new Error('Could not fetch the data for that resource!');
+        }
+        return res.json();
       })
-      .then (data => {
-        setBlogs(data);
+      .then(data => {
         setIsPending(false);
+        setBlogs(data);
+        setError(null);
       })
-    }, [])
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [])
 
   return (
     <div className="home">
-      { isPending && <div>Loading...</div>}
-      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
